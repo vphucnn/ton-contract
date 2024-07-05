b944ea9e4d2b84b4895fb317447ac14b4f4bb5bd
vphucnn <140361269+vphucnn@users.noreply.github.com>
2024-07-05 10:16:32 +0700

    add call get set

--- wrappers/Counter.ts
+++ wrappers/Counter.ts
@@ -19,4 +19,20 @@ export default class Counter implements Contract {
       bounce: false
     });
   } 
+
+  async getCounter(provider: ContractProvider) {
+    const { stack } = await provider.get("counter", []);
+    return stack.readBigNumber();
+  }
+
+  async sendIncrement(provider: ContractProvider, via: Sender) {
+    const messageBody = beginCell()
+      .storeUint(1, 32) // op (op #1 = increment)
+      .storeUint(0, 64) // query id
+      .endCell();
+    await provider.internal(via, {
+      value: "0.002", // send 0.002 TON for gas
+      body: messageBody
+    });
+  }
 }
